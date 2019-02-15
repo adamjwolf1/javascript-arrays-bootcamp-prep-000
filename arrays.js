7a3d3be3f84a93db86887691006ce21b2ca9037c
Adam Wolf <adamjwolf1@gmail.com>
2019-02-15 13:04:53 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+//var candyString = "foo"
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
